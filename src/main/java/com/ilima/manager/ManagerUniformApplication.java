94568efc604fd5cd61d5d01b7a45aec557cc6bbb
DESKTOP-BQKTILS\israe <israel_lima_16@hotmail.com>
2018-12-17 22:58:06 -0300

    Projeto Criado Dependenciais Deafault Injetadas

---
+++ src/main/java/com/ilima/manager/ManagerUniformApplication.java
@@ -0,0 +1,14 @@
+package com.ilima.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagerUniformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagerUniformApplication.class, args);
+	}
+
+}
+
