e7bad02538922da04391dc9e2ceb07531b52b5f7
DESKTOP-BQKTILS\israe <israel_lima_16@hotmail.com>
2018-12-19 19:18:36 -0300

    DB Service Ajuste pt1

--- src/main/java/com/ilima/manager/ManagerUniformApplication.java
+++ src/main/java/com/ilima/manager/ManagerUniformApplication.java
@@ -12,10 +12,18 @@ import com.ilima.manager.domains.Boina;
 import com.ilima.manager.domains.Calca;
 import com.ilima.manager.domains.Calcado;
 import com.ilima.manager.domains.Camisa;
+import com.ilima.manager.domains.Entrega;
+import com.ilima.manager.domains.Funcionario;
+import com.ilima.manager.domains.ItemEntrega;
 import com.ilima.manager.domains.ItemPedido;
 import com.ilima.manager.domains.Pedido;
 import com.ilima.manager.enuns.EstadoPedido;
 import com.ilima.manager.enuns.TamanhoItem;
+import com.ilima.manager.repositories.DevolucaoRepository;
+import com.ilima.manager.repositories.EntregaRepository;
+import com.ilima.manager.repositories.FuncionarioRepository;
+import com.ilima.manager.repositories.ItemDevolucaoRepository;
+import com.ilima.manager.repositories.ItemEntregaRepository;
 import com.ilima.manager.repositories.ItemPedidoRepository;
 import com.ilima.manager.repositories.ItemRepository;
 import com.ilima.manager.repositories.PedidoRepository;
@@ -29,7 +37,16 @@ public class ManagerUniformApplication implements CommandLineRunner {
 	private PedidoRepository repoPedido;
 	@Autowired
 	private ItemPedidoRepository repoItemPedido;
-	
+	@Autowired
+	private EntregaRepository repoEntrega;
+	@Autowired
+	private ItemEntregaRepository repoItemEntrega;
+	@Autowired
+	private DevolucaoRepository repoDevolucao;
+	@Autowired
+	private ItemDevolucaoRepository repoItemDevolucao;
+	@Autowired
+	private FuncionarioRepository repoFuncionario;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ManagerUniformApplication.class, args);
@@ -47,27 +64,48 @@ public class ManagerUniformApplication implements CommandLineRunner {
 
 		Pedido p1 = new Pedido(null, new Date(), EstadoPedido.PENDENTE);
 		Pedido p2 = new Pedido(null, new Date(), EstadoPedido.PENDENTE);
-		
-		ItemPedido i1 = new ItemPedido(p1,c1,15);
-		ItemPedido i2 = new ItemPedido(p1,c3,20);
-		
-		ItemPedido i3 = new ItemPedido(p2,c2,8);
-		ItemPedido i4 = new ItemPedido(p2,c4,7);
-		
-		p1.getItens().addAll(Arrays.asList(i1,i2));
-		p2.getItens().addAll(Arrays.asList(i3,i4));
-		
+
+		ItemPedido i1 = new ItemPedido(p1, c1, 15);
+		ItemPedido i2 = new ItemPedido(p1, c3, 20);
+
+		ItemPedido i3 = new ItemPedido(p2, c2, 8);
+		ItemPedido i4 = new ItemPedido(p2, c4, 7);
+
+		p1.getItens().addAll(Arrays.asList(i1, i2));
+		p2.getItens().addAll(Arrays.asList(i3, i4));
+
 		c1.getPedidos().addAll(Arrays.asList(i1));
 		c2.getPedidos().addAll(Arrays.asList(i3));
 		c3.getPedidos().addAll(Arrays.asList(i2));
 		c4.getPedidos().addAll(Arrays.asList(i4));
-		
-		
-		repoPedido.saveAll(Arrays.asList(p1,p2));
-		repoItemPedido.saveAll(Arrays.asList(i1,i2,i3,i4));
-		
-		
 
+		repoPedido.saveAll(Arrays.asList(p1, p2));
+		repoItemPedido.saveAll(Arrays.asList(i1, i2, i3, i4));
+
+		Funcionario f1 = new Funcionario(null, "Robson Cardoso Silva", "Segurança");
+		Funcionario f2 = new Funcionario(null, "Luan Souza Oliveira", "Segurança");
+		Funcionario f3 = new Funcionario(null, "Josenilton Soares Carvalho", "Supervisor");
+
+		repoFuncionario.saveAll(Arrays.asList(f1, f2, f3));
+
+		Entrega e1 = new Entrega(null, new Date());
+		e1.setFuncionario(f1);
+		ItemEntrega ie1 = new ItemEntrega(e1, c1, 1);
+		ItemEntrega ie2 = new ItemEntrega(e1, c2, 2);
+
+		c1.getEntrega().addAll(Arrays.asList(ie1));
+		c2.getEntrega().addAll(Arrays.asList(ie2));
+		e1.getItem().addAll(Arrays.asList(ie1, ie2));
+
+		Entrega e2 = new Entrega(null, new Date());
+		e2.setFuncionario(f2);
+		ItemEntrega ie3 = new ItemEntrega(e2, c3, 1);
+
+		c3.getEntrega().addAll(Arrays.asList(ie3));
+		e2.getItem().addAll(Arrays.asList(ie3));
+
+		repoEntrega.saveAll(Arrays.asList(e1));
+		repoItemEntrega.saveAll(Arrays.asList(ie1, ie2));
 	}
 
 }
