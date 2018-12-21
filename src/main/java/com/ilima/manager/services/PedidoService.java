474d3f99eb50362e4e871318e738b00b10247e60
DESKTOP-BQKTILS\israe <israel_lima_16@hotmail.com>
2018-12-21 10:08:50 -0300

    Handler ObjectNotFoundException implementado

--- src/main/java/com/ilima/manager/services/PedidoService.java
+++ src/main/java/com/ilima/manager/services/PedidoService.java
@@ -4,7 +4,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import com.ilima.manager.services.exceptions.ObjectNotFoundException;
 import com.ilima.manager.domains.Pedido;
 import com.ilima.manager.repositories.PedidoRepository;
 
@@ -13,10 +13,15 @@ public class PedidoService {
 
 	@Autowired
 	private PedidoRepository repo;
+	/*
+	 * public Pedido find(Integer id) throws Exception { Optional<Pedido> obj =
+	 * repo.findById(id); return obj.orElseThrow(() -> new Exception()); }
+	 */
 
-	public Pedido find(Integer id) throws Exception {
+	public Pedido find(Integer id) {
 		Optional<Pedido> obj = repo.findById(id);
-		return obj.orElseThrow(() -> new Exception());
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
 	}
 
 }
